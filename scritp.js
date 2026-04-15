7ce698180fbe79b8bf0f2e87bc9da2dae7c9916f
Emily7980 <1720980@senati.pe>
2026-04-15 14:42:10 -0500

    Hmtl y css finished

--- scritp.js
+++ scritp.js
@@ -4,4 +4,15 @@ const nombre = "Emily Torres"
 const precio = 99.99;
 var marca = "Dell";
 console.log(nombre,precio,edad)
-edad = 40;
+edad += 5;
+console.log(edad);
+console.log("suma: ", 5+5);
+if(edad >= 18){
+    console.log("Mayor de edad");
+}else{
+    console.log("Menor de edad");
+}
+const frutas = ["manzana","pera","naranja","fresa"];
+frutas.map((frutas)=>{
+    console.log(frutas);
+});
